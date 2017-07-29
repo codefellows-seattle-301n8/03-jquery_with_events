6def0cdc86ed369ed7bae3ad7fd747d08203514a
Joel Epstein <joel.epstein2@gmail.com>
2017-07-29 09:38:31 -0700

    added jquery to fadeIn

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -45,7 +45,8 @@ articleView.handleAuthorFilter = function() {
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
-
+      $('article').fadeIn();
+      $('article.template').hide();
     }
     $('#category-filter').val('');
   });
