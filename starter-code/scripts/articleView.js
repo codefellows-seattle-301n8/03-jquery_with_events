4a391d64562b537b514d4cbb55a2131f8a0d4342
Joel Epstein <joel.epstein2@gmail.com>
2017-07-29 13:36:51 -0700

    added link to new js file

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -90,7 +90,6 @@ articleView.setTeasers = function() {
   //       "Read On" link once it has been clicked. Be sure to prevent the default link-click action!
   //       Ideally, we'd attach this as just 1 event handler on the #articles section, and let it
   //       process any .read-on clicks that happen within child nodes.
-  $('.article-body *:nth-of-type(n+2)').hide();
   $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
     if ($(this).text() === 'Read on →') {
