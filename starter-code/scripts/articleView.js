7129a61ff2a6e186026cf1aa68e6527fe0fb8e69
Joel Epstein <joel.epstein2@gmail.com>
2017-07-29 09:43:27 -0700

    basically copied my code from auther filter and applied it to category filter

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -57,7 +57,16 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
-
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      $('article').hide();
+      $('article[data-category="${$(this).val()}"]').fadeIn();
+    } else {
+      $('article').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
 };
 
 articleView.handleMainNav = function() {
